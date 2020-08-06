5bc61f33c2c7d26967ecc3ff230c2e42ecca879e
plungerman <plungerman@gmail.com>
2020-08-06 11:06:30 -0500

    finalized the shell script for reminding fac/staff to execute their daily health check

--- redpanda/bin/facstaff.py
+++ redpanda/bin/facstaff.py
@@ -11,22 +11,21 @@ import django
 django.setup()
 
 from django.conf import settings
-from django.core.cache import cache
 from django.core.validators import validate_email
 from django.urls import reverse_lazy
 from djimix.core.database import get_connection
 from djimix.core.database import xsql
 from djimix.people.utils import get_peeps
 from djimix.core.encryption import encrypt
-
-import logging
-
-logger = logging.getLogger('debug_logfile')
+from djtools.utils.mail import send_mail
 
 
 def main():
     """Main function description."""
-    for peep in get_peeps('facstaff'):
+    request = None
+    frum = settings.DEFAULT_FROM_EMAIL
+    subject = "[Health Check] Daily Reminder"
+    for counter, peep in enumerate(get_peeps('facstaff')):
         #print(get_uuid(peep['email']))
         #print(peep['email'])
         email = peep['email']
@@ -36,13 +35,19 @@ def main():
             results = xsql(sql, connection)
             row = results.fetchone()
             if row:
-                print(
-                    "https://{0}{1}?uid={2}".format(
-                        settings.SERVER_URL,
-                        settings.ROOT_URL,
-                        encrypt(row[0]),
-                    ),
-                    row[4],
+                earl = 'https://{0}{1}?uid={2}'.format(
+                    settings.SERVER_URL,
+                    settings.ROOT_URL,
+                    encrypt(row[0]),
+                )
+                context_data = {'earl': earl, 'peep': peep}
+                send_mail(
+                    request,
+                    [email],
+                    subject,
+                    frum,
+                    'email_reminder.html',
+                    context_data,
                 )
 
 
