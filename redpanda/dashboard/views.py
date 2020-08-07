8f7374fbebcab6596b4c13e58edf5c0dadf20b6f
plungerman <plungerman@gmail.com>
2020-08-06 22:09:13 -0500

    dashboard for reporting

--- redpanda/dashboard/views.py
+++ redpanda/dashboard/views.py
@@ -4,12 +4,23 @@
 
 from django.conf import settings
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from djimix.decorators.auth import portal_auth_required
+from redpanda.core.models import HealthCheck
 
 
+@portal_auth_required(
+    session_var='REDPANDA_AUTH',
+    redirect_url=reverse_lazy('access_denied'),
+)
 def home(request):
     """Dashboard home."""
     return render(
-        request, 'dashboard/home.html', {'foobar': settings.FOO_BAR},
+        request,
+        'dashboard/home.html',
+        {
+            'czechs': HealthCheck.objects.all(),
+        },
     )
 
 
--- redpanda/static/js/main.js
+++ redpanda/static/js/main.js
@@ -9,6 +9,32 @@ $(function() {
       $("#id_negative").prop("checked", false);
     };
   });
+  /* datatables initialization */
+  var gearupTable = $('#data-table').DataTable({
+    'lengthMenu': [
+      [25, 50, 100, 250, 500, 1000, 2000, -1],
+      [25, 50, 100, 250, 500, 1000, 2000, 'All']
+    ],
+    drawCallback: function() {
+        $('[data-toggle="popover"]').popover({
+            trigger: 'hover',
+            'placement': 'right'
+        });
+    },
+    'columnDefs': [
+      { targets: 'no-sort', orderable: false }
+    ],
+    order: [[1, "asc"]],
+    dom: 'lfrBtip',
+    buttons: [
+      {
+        extend: 'excelHtml5',
+        exportOptions: {
+          columns: ':visible'
+        }
+      }
+    ]
+  });
   /* override the submit event for the alert form to handle some things */
   $('form#health-check').submit(function(){
     // disable submit button after users clicks it
