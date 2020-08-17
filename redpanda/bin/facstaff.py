4279cf0b4f90ba1c62c6bc2106e6abad09c48f98
plungerman <plungerman@gmail.com>
2020-08-17 09:07:14 -0500

    test run for email reminder for faculty and staff

--- redpanda/bin/facstaff.py
+++ redpanda/bin/facstaff.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import json
 import os
+import requests
 import sys
 
 # env
@@ -23,18 +25,35 @@ def main():
     request = None
     frum = settings.DEFAULT_FROM_EMAIL
     subject = "[Health Check] Daily Reminder"
-    for counter, peep in enumerate(get_peeps('facstaff')):
-        email = peep['email']
-        sql = "SELECT * FROM fwk_user WHERE HostID like '%{}'".format(peep['cid'])
+
+    sql = 'SELECT * FROM provisioning_vw WHERE id in {0}'.format(
+        settings.REDPANDA_TEST_CIDS,
+    )
+    print(sql)
+    with get_connection() as connection:
+        peeps = xsql(sql, connection).fetchall()
+    #peeps = get_peeps('facstaff')
+    for peep in peeps:
+        #email = peep['email']
+        email = '{0}@carthage.edu'.format(peep[3])
+        #sql = "SELECT * FROM fwk_user WHERE HostID like '%{}'".format(peep['cid'])
+        sql = "SELECT * FROM fwk_user WHERE HostID like '%{}'".format(peep[0])
         with get_connection(settings.MSSQL_EARL, encoding=False) as connection:
             results = xsql(sql, connection)
             row = results.fetchone()
             if row:
-                earl = 'https://{0}{1}?uid={2}'.format(
-                    settings.SERVER_URL,
-                    settings.ROOT_URL,
-                    encrypt(row[0]),
+
+                earl = 'https://{0}{1}{2}?uid={3}'.format(
+                    settings.REDPANDA_SERVER_URL,
+                    settings.REDPANDA_ROOT_URL,
+                    settings.REDPANDA_SHORT_URL_API,
+                    row[0],
                 )
+                print(earl)
+                response = requests.get(earl)
+                jason_data = json.loads(response.text)
+                earl = jason_data['lynx']
+                print(earl)
                 context_data = {'earl': earl, 'peep': peep}
                 send_mail(
                     request,
